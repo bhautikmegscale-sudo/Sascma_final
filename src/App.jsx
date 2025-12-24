9b8c7d80604e4e0e4682a1556c091ced2ed8960c
Chintan Megascale <chintan.megascale@gmail.com>
2025-12-24 12:06:37 +0530

    HASH ROUTER Added

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@
 
 
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
