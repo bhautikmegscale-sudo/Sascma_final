f7b74507fae4ce8dbb5e3fdca308e8af8e2ab0ba
Chintan Megascale <chintan.megascale@gmail.com>
2025-12-24 12:06:02 +0530

    HASH ROUTER remove

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@
 
 
 import React, { useEffect, useState } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
